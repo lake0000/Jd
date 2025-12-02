16ed74f9206c67b2b3468e6c9d59be9206fed223
lake0000 <Moored_cloud@outlook.com>
2025-12-02 22:50:41 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/你的仓库名/', // 例如仓库叫 my-portfolio，这里就填 '/my-portfolio/'
+  // 重要：把下面的 'Jd' 替换成你 GitHub 仓库的实际名字
+  base: '/JD/', 
 })
